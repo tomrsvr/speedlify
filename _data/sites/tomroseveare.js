c8b7356a868de38af16edb1d8d6d5902543fc6c3
tomrsvr <4099754+tomrsvr@users.noreply.github.com>
2024-06-20 20:14:51 +0900

    Sites change

--- _data/sites/tomroseveare.js
+++ _data/sites/tomroseveare.js
@@ -1,26 +1,24 @@
 module.exports = {
-	description: "Front-end Testing Tools",
+	name: "tomroseveare.com", // optional, falls back to object key
+	description: "Tom Roseveare — Portfolio",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		frequency: 60 * 11 + 30, // 11h, 30m
+		// runs: 1,
+		frequency: 60 * 23, // 23 hours
+		// frequency: 60 * 11 + 30, // 11h, 30m
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
 		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "site",
+		freshChrome: "site"
 	},
 	urls: [
-		"https://eslint.org/",
-		"https://qunitjs.com/",
-		"https://karma-runner.github.io/latest/index.html",
-		"https://gulpjs.com/",
-		"https://webhint.io/",
-		"https://gruntjs.com/",
-		"https://theintern.io/",
-		"https://istanbul.js.org/",
-		"https://webdriver.io/",
-		"https://mochajs.org/"
+		"https://www.tomroseveare.com/",
+		"https://www.tomroseveare.com/about/",
+		"https://www.tomroseveare.com/work/",
+		"https://www.tomroseveare.com/contact/",
+		"https://www.tomroseveare.com/blog/",
 	]
 };
