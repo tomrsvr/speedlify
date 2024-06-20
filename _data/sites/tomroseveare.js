fa90929155b98a9f8751fcd83a989ad2cc5f4454
tomrsvr <4099754+tomrsvr@users.noreply.github.com>
2024-06-20 20:18:03 +0900

    Update tomroseveare.js

--- _data/sites/tomroseveare.js
+++ _data/sites/tomroseveare.js
@@ -10,7 +10,7 @@ module.exports = {
 		// frequency: 60 * 11 + 30, // 11h, 30m
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
-		//           each site in every run (it’s faster)
+		//           each site in every run (it’s faster) 
 		// Use "site" if sites are all on the same origin and share assets.
 		freshChrome: "site"
 	},
